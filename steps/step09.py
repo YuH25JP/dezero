b5d21d64d6b7cb74079b90e39f75771ff6c0753a
YuH25JP <cppyuh@gmail.com>
2025-11-03 09:14:26 +0900

    step 09 completed

---
+++ steps/step09.py
@@ -0,0 +1,104 @@
+from __future__ import annotations  # lazy evaluation
+
+import numpy as np
+import numpy.typing as npt
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+
+    return x
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)} is not supported")
+
+        self.data = data
+        self.grad: npt.ArrayLike | None = None
+        self.creator: Function | None = None
+
+    def set_creator(self, func: Function):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            if f is None:
+                raise ValueError("The function is None.")
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x**2
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+def square(x):
+    f = Square()
+    return f(x)
+
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+if __name__ == "__main__":
+    x = Variable(np.array(0.5))
+    y = square(exp(square(x)))
+
+    # y.grad = np.array(1.0)
+    y.backward()
+    print(x.grad)
