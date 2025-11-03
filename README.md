8ac050aa7d85157491109eb66dcf24e3a46987a0
YuH25JP <cppyuh@gmail.com>
2025-11-03 14:22:07 +0900

    enabled pytest and put tests in /tests/ directory

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Dezero
 This is a practice repository for [DeZero](https://github.com/oreilly-japan/deep-learning-from-scratch-3) from [ゼロから作るDeep Learning ❸](https://www.oreilly.co.jp/books/9784873119069/).
+
+## Run tests
+The testing framework is `pytest`.
+
+Tests are placed in `tests/` directory.
+
+To run all tests placed in `tests/`, run the following command:
+```bash
+uv run pytest
+```
---
+++ tests/step10/test_1_to_10.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from steps.step10 import Variable, square, numerical_diff
+
+
+class TestSquare:
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        assert y.data == expected
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        assert x.grad == expected
+
+    def test_gradient_check(self):
+        """Test by comparing two different gradient values by autodiff and numerical algorithms."""
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+
+        num_grad = numerical_diff(square, x)
+
+        flg = False
+        if x.grad is not None:
+            flg = np.allclose(x.grad, num_grad)
+
+        assert flg
