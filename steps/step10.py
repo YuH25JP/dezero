1998373fd608ace76af1e4bdffc403af4472016e
YuH25JP <cppyuh@gmail.com>
2025-11-03 09:37:30 +0900

    step 10 completed

---
+++ steps/step10.py
@@ -0,0 +1,29 @@
+import numpy as np
+import unittest
+from .step09 import Variable, square, numerical_diff
+
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        """Test by comparing two different gradient values by autodiff and numerical algorithms."""
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = False
+        if x.grad is not None:
+            flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
