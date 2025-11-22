cd2d9f19d96d2481500832b61488403505faea8d
YuH25JP <cppyuh@gmail.com>
2025-11-22 13:49:21 +0900

    step17 completed

---
+++ steps/step17.py
@@ -0,0 +1,138 @@
+from __future__ import annotations  # lazy evaluation
+
+import weakref
+
+import numpy as np
+import numpy.typing as npt
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+
+    return x
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)} is not supported")
+
+        self.data = data
+        self.grad: npt.ArrayLike | None = None
+        self.creator: Function | None = None
+        self.generation = 0
+
+    def set_creator(self, func: Function):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+
+            # Make sure that f is not None
+            if f is None:
+                raise ValueError("The function is None.")
+
+            # gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+    def cleargrad(self):
+        self.grad = None
+
+
+class Function:
+    def __call__(self, *inputs: Variable) -> list[Variable] | Variable:
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = [weakref.ref(output) for output in outputs]
+
+        # If `outputs` consists of only 1 element, the content itself is returned,
+        # otherwise whole tuple is returned.
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+
+def add(x0, x1):
+    f = Add()
+    return f(x0, x1)
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x**2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    return Square()(x)
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        x = Variable(np.random.randn(1000))
+        y = square(square(square(x)))
